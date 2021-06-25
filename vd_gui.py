e88e93bf920b47acf7deb475ec4b204568a4a2f5
Garth Martinsen <garth.martinsen@gmail.com>
2021-06-25 00:39:08 -0700

    Commit for voltage_divider.py and vd_gui.py on 6/25/2021 @ 0:18  Just past midnight. in the GUI file vd_gui.py : •Added method vd_gui.get_max_mw() to set the max_mw Input in the GUI. Populated value in the View. •Commented out print statements. •Added an Image: Schematic.png to show meaning of Table values on a schematic In the file: voltage_divider.py •Changed parameter tolerance to just v2_lo and v2_hi  so meets_specs tests can use: ⁃ v2_lo <= v2 <= v2_hi and ⁃ p1 < max_mw and p1<max_mw ⁃I set v2_low to 1.0 so that difficult designs would have some candidates. •Used f-strings to format the floats and ints for better display. •Changed namedtuple VD to include voltages for display in the table •Modified tests as changes were made to the code.

--- vd_gui.py
+++ vd_gui.py
@@ -1,55 +1,82 @@
 # vd_gui.py
 
 import PySimpleGUI as sg
-import re
 import os
 import voltage_divider as vdr
+import re
 
 # global variables
-#max_mw = 0
-#path = ''
+# max_mw = 0
+# path = ''
+
+
+def get_max_mw(path):
+    """
+    returns 250 or 500 mw, etc depending on the filename.
+    If another set of resistors exist with different power ratings,
+    A file should be created with a title reflecting the rating.
+    """
+    q = re.findall('quarter', path)
+    h = re.findall('half', path)
+    if len(q) > 0:
+        max_mw = 250
+    elif len(h) > 0:
+        max_mw = 500
+    return max_mw
 
 
 def get_resistor_file_path(values):
-    print("in function: get_resistor_file_path")
+    # print("in function: get_resistor_file_path")
     path = sg.popup_get_file("Select correct File",
                              file_types=(('CSV', '*.csv'),),
                              initial_folder=os.getcwd()
                              )
+    if path:
+        window["max_mw"].update(get_max_mw(path))
     return path
 
 
 def get_candidates(values):
     path = values["path"]
     vin1 = values["Vin_1"]
-    print(f'get_candidates called. Values for path: {path} and Vin: {vin1}')
+    # print(f'get_candidates called(path: {path} and Vin: {vin1})')
     candidates = vdr.compute(vin1, path)
-    print(f'Returned {len(candidates)} candidates')
+    # print(f'Returned {len(candidates)} candidates')
     window["candidate_table"].update(values=candidates)
 
 
 input_layout = [[
-                sg.Text("Vin"), sg.Input("", size=(7, 1), key="Vin_1"),
-                sg.Button("Resistors"), 
+                sg.Button("Resistors"),
                 sg.Input("", size=(80, 1), key="path"),
+                sg.Text("Vin"), sg.Input("", size=(7, 1), key="Vin_1"),
                 sg.Button("Compute"),
                 ]]
 # VD = namedtuple("VD", "Vin V1 V2 Deviance R1 R2 Pow1_mw Pow2_mw A2D")
-
-headings = ["Vin", "V1", "V2", "Dev", "R1", "R2", "P1mw","P2mw","A2D"]
+root = os.getcwd()
+headings = ["Vin", "V1", "V2", "Dev", "R1_ohms",
+            "R2_ohms", "P1_mw", "P2_mw", "A2D"]
 table_layout = [[sg.Table(values=[], headings=headings, num_rows=6,
                           auto_size_columns=False, key='candidate_table',
                           alternating_row_color='Grey',
-                          col_widths=list(map(lambda x: 3*( len(x) + 1), headings)),  # noqa: E501
-                          hide_vertical_scroll=True)]]
+                          col_widths=list(map(lambda x: 2 * (len(x) + 1), headings)),  # noqa: E501
+                          hide_vertical_scroll=True), sg.Text("Max_mw "),
+                sg.Input("", size=(5, 1), key="max_mw"), ]]
+schematic = [[sg.Image(filename=root + "/Schematic.png",
+                       size=(335, 435), visible=True,)]]  # key="Schematic",
 output_layout = [[sg.Column(table_layout, scrollable=True, size=(1000, 110))]]
 
-input_frame = sg.Frame("Inputs", input_layout, size=(5, 1), visible=True)
-output_frame = sg.Frame("Outputs", output_layout, size=(5, 1), visible=True)
-layout = [[input_frame]], [[output_frame]]
+input_frame = sg.Frame("Inputs", input_layout, visible=True)
+output_frame = sg.Frame("Outputs", output_layout, visible=True)
+layout = [[[[input_frame]], [[output_frame]], [[schematic]]]]
+
+# sg.Debug("Debug")
 
 
-window = sg.Window("Voltage Divider", layout, font="Courier 16", resizable=True)
+window = sg.Window("Voltage Divider", layout,
+                   font="Courier 14", return_keyboard_events=True,
+                   resizable=True)
+# sg.show_debugger_window(location=(0, 0))
+
 while True:
     event, values = window.read()
     if event in (sg.WIN_CLOSED, 'Exit'):
@@ -57,6 +84,10 @@ while True:
     elif event == "Resistors":
         path = get_resistor_file_path(values)
         window["path"].update(path)
+    elif event in ("Tab:805306377", "Return:603979789"):
+        print("Either a tab or returnKey has been entered")
+        window["Compute"].click()
     elif event == "Compute":
-        print(f'Getting ready to call vdr.compute with:{values}')
+        # print(f'Getting ready to call vdr.compute with:{values}')
         get_candidates(values)
+    # print(event, values)
--- voltage_divider.py
+++ voltage_divider.py
@@ -11,15 +11,15 @@ DesignGoals = namedtuple("DesignGoals", "vin v2_hi v2_lo max_mw")
 
 # --------------global variables-------------------------------
 v2_hi = 4.95  # a2d = 1012
-v2_lo = 4.00  # a2d = 951
+v2_lo = 1.00  # a2d = 205
 max_mw = 0   # set in process
 
 
 def load_resistor_set(path):
     """
-Loads the set of resistors available for use into a set. The best Combination
-of two of these will be used in the selected voltage divider.
-"""
+    Loads the set of resistors available for use into a set. The best 
+    Fit can be used in the selected voltage divider.
+    """
     resistorSet = set()
     with open(path) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
@@ -45,14 +45,14 @@ def build_voltage_divider(design_goals, r1, r2):
     vin = design_goals.vin
     v1 = vin * (1 - fract)
     v2 = design_goals.vin * fract
-    p1 = int(v1**2 / r1 * 1000 ) # mW
-    p2 = int(v2**2 / r2 * 1000 ) # mW
+    p1 = int(v1**2 / r1 * 1000)  # mW
+    p2 = int(v2**2 / r2 * 1000)  # mW
     a2d = int(f'{v2/5*1023:.0f}')
     dev = design_goals.v2_hi - v2
     # size for display
-    vin= float(f'{vin:4.2f}')
-    v1= float(f'{v1:4.2f}')
-    v2= float(f'{v2:4.2f}')
+    vin = float(f'{vin:4.2f}')
+    v1 = float(f'{v1:4.2f}')
+    v2 = float(f'{v2:4.2f}')
     dev = float(f'{dev:6.4f}')
     # VD = namedtuple("VD", "Vin V1 V2 Deviance R1 R2 Pow1_mw Pow2_mw A2D")
     return VD(vin, v1, v2, dev, r1, r2, p1, p2, a2d)
@@ -60,7 +60,7 @@ def build_voltage_divider(design_goals, r1, r2):
 
 def get_max_mw(path):
     """
-    returns 250 or 500 mw depending on the filename.
+    returns 250 or 500 mw, etc depending on the filename.
     If another set of resistors exist with different power ratings,
     A file should be created with a title reflecting the rating.
     """
@@ -127,12 +127,13 @@ def compute(Vin, path):
     design_goals = DesignGoals(float(Vin), v2_hi, v2_lo, max_mw)
     choices = find_choices(design_goals, resistor_set)
     # print(choices)
-    end = min(5,len(choices))
+    end = min(5, len(choices))
     return choices[0:end]
+
 
 # --------------------unit testing-----------------------------
 # path = '/Users/garth/Programming/python3/VD3/quarter_watt.csv'
-path = os.getcwd() + '/resistors_quarter.csv';
+path = os.getcwd() + '/quarter_watt.csv'
 
 # "DesignGoals", "vin v2_hi v2_lo max_mw")
 design_goals = DesignGoals(25.25, v2_hi, v2_lo, 250)
@@ -142,8 +143,8 @@ resistorSet = load_resistor_set(path)
 def test_compute():
     fd = compute(design_goals.vin, path)
     l1 = len(fd)
-    #print(f' Num candidates: {l1}')
-    assert(0 <= l1 <= 5)  # replace with better test. 
+    # print(f' Num candidates: {l1}')
+    assert(0 <= l1 <= 5)  # replace with better test.
 
 
 def test_find_choices():
@@ -154,24 +155,19 @@ def test_find_choices():
     candidates = find_choices(design_goals, resistorSet)
     assert candidates[0].Deviance < candidates[1].Deviance
 
+
+"""
+not a good test because the test_vd is rounded for display so it will not match any of the vds generated.  # noqa: E501
+
     def test_find_choices_2():
-        """
-        bug when running with vd_gui.py, this VD should not meet specs. The r values are wonky
-        VD(Vin=25.5, V1=20.7, V2=4.8, Deviance=0.1511, R1=220000, R2=51000, Pow1_mw=1, Pow2_mw=0, A2D=982)
-        but it is returned as an acceptable VD candidate.
-        """
-        test_vd=VD(25.5, 20.7, 4.8, 0.1511, 220000, 51000, 1, 0, 982) 
-        candidates = vdr.compute(25.25,path)   
+
+       # bug when running with vd_gui.py, this VD should not meet specs. 
+
+      
+        test_vd = VD(25.5, 20.7, 4.8, 0.1511, 220000, 51000, 1, 0, 982)
+        candidates = compute(25.25, path)
         assert test_vd not in candidates
-
-
-def test_load_resistor_set():
-    """
-    Ensure that the loaded resistors have the same count as the file.
-    """
-    resistorSet = load_resistor_set(path)
-    # replace with better test. Adding a resistor value would break test.
-    assert(len(resistorSet) == 37)
+"""
 
 
 def test_design_meets_specs():
@@ -183,11 +179,11 @@ def test_design_meets_specs():
     assert Pass
 
 
-
 def test_design_meets_specs_3():
     """
-    This is to check for the bug in a gui run. I had left off a zero in 220000. Not a bug after all.
+    This is to check for the bug in a gui run. I had left off a zero in 220000. 
+    Not a bug after all.
     """
-    # r1=220000  r2=51000 
+    # r1=220000  r2=51000
     Pass = design_meets_specs(design_goals, 220000, 51000)
     assert (Pass)
