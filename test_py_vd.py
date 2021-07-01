d7995144cd7ccc932b8993f92370443202183aff
Garth Martinsen <garth.martinsen@gmail.com>
2021-07-01 11:30:30 -0700

    Replaced unittest with pytest. Simpler and just as effective.

---
+++ test_py_vd.py
@@ -0,0 +1,123 @@
+# file: test_py_vd.py
+
+import voltage_divider as vdr
+import os
+import pytest
+from collections import namedtuple
+
+"""
+this is to investigate pytest which is simpler than unittest. The names of the
+test methods is the same but self is not passed and subclassing TestCase is not 
+required. For test fixtures to work, all named tuples used in testing need to be 
+defined in the test file and namedtuples need to be imported: 
+    from collections import namedtuples
+If a fixture is needed in the fixture definition, it needs to be passed as an 
+argument. 
+See fixture resistorSet(...) below.
+"""
+DesignGoals = namedtuple("DesignGoals", "vin v2_hi v2_lo max_mw")
+VD = namedtuple("VD", "Vin V1 V2 Deviance R1 R2 Pow1_mw Pow2_mw A2D")
+
+
+#  --------- globals and test fixtures-----------------
+v2_hi = vdr.v2_hi
+v2_lo = vdr.v2_lo
+max_mw = 250
+
+#path = os.getcwd() + '/quarter_watt.csv'
+#resistorSet = vdr.load_resistor_set(path)
+#design_goals = vdr.DesignGoals(25.2, v2_hi, v2_lo, max_mw)
+
+@pytest.fixture
+def path():
+    return os.getcwd() + '/quarter_watt.csv'
+
+
+@pytest.fixture
+def resistorSet(path):
+    return vdr.load_resistor_set(path)
+
+@pytest.fixture
+def design_goals():
+    return DesignGoals(25.2, v2_hi, v2_lo, 250)
+
+# -------------------tests---------------------------------
+
+
+def test_fixture(design_goals):
+    print("This is to test the test fixture: design_goals ")
+    A = design_goals.vin == 25.2 
+    B = design_goals.v2_hi == vdr.v2_hi 
+    C = design_goals.v2_lo == vdr.v2_lo 
+    D = design_goals.max_mw == 250
+    assert A and B and C and D
+
+
+def test_compute(design_goals, path):
+    """
+    ---Test end to end.Given vin and path, ensure returns 5 or less vds
+    """
+    vds = vdr.compute(design_goals.vin, path)
+    l1 = len(vds)
+    # print(f' Num candidates: {l1}')
+    assert(0 <= l1 <= 5)  # replace with better test.
+
+
+def test_find_choices(design_goals, resistorSet):
+    """
+   ---Ensure vds returned are sorted ascending by Deviance from target V2
+   """
+    # assert 6 == len(find_choices(design_goals, resistorSet))
+    candidates = vdr.find_choices(design_goals, resistorSet)
+    assert(candidates[0].Deviance <= candidates[1].Deviance)
+
+
+def test_design_meets_specs(design_goals):
+    """
+    ---Ensure that good inputs Pass specs :
+    """
+    Pass = vdr.design_meets_specs(design_goals, 2000, 470)
+    assert Pass
+
+
+def test_design_meets_specs_2(design_goals):
+    """
+    ---Ensure bad inputs Fail specs :
+    """
+    Pass = vdr.design_meets_specs(design_goals, 200, 47)
+    assert not Pass
+
+
+def test_design_meets_specs_3(design_goals):
+    """
+    ---This is to check a bug in a gui run. Not a bug after all.
+    """
+    # r1=220000  r2=51000
+    assert vdr.design_meets_specs(design_goals, 220000, 51000)
+
+
+def test_get_max_mw(path):
+    """
+    ---Ensures that the correct wattage is set from the path to the resistors.
+    """
+    assert vdr.get_max_mw(path) == 250
+
+
+def test_load_resistor_set(path):
+    """
+    ---Ensures that the resistor set is loaded correctly given path.
+    """
+    resistorSet = vdr.load_resistor_set(path)
+    assert isinstance(resistorSet, set) and len(resistorSet) == 37
+
+
+def test_build_voltage_divider(design_goals):
+    """
+    ---Tests that the namedtuple VD is returned correctly
+     # VD = namedtuple("VD", "Vin V1 V2 Deviance R1 R2 Pow1_mw Pow2_mw A2D")
+     # DesignGoals = namedtuple("DesignGoals", "vin v2_hi v2_lo max_mw")
+
+    """
+    vd = vdr.build_voltage_divider(design_goals, 2000, 470)
+
+    assert vd.Vin == design_goals.vin and vd.V1 == 20.4 and vd.V2 == 4.8 and vd.Pow1_mw == 208 and vd.Pow2_mw == 48 and vd.A2D == 981  # noqa: E501
