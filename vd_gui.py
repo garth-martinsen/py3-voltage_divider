2b793d359b1309087b217109eb3cad08fa9ec230
Garth Martinsen <garth.martinsen@gmail.com>
2021-07-10 01:00:44 -0700

    Replaced the elif event ==x : handling with a dictionary that acts like a switch and does nothing for non-important events. ; Modified the layout. Added readonly Inputs to show the V2 constraints and the max_mw limit. There are now 4 frames: “inputs“, “Outputs”, “Schematic”,“Constraints”.  There are now 2 global variables: “proj_dir”, and “values”. The “values” were needed so that the handler methods did not have to pass any args.  Goal: simplify the getFile process. Too many popups. Maybe try browse_file, etc.

--- vd_gui.py
+++ vd_gui.py
@@ -1,9 +1,12 @@
-# vd_gui.py
+# file: vd_gui.py
 
 import PySimpleGUI as sg
-import os
 import voltage_divider as vdr
-#  import re
+import os
+
+#  -----global variables-----
+values = dict()
+proj_dir = os.getcwd()  #  If your resistor files are not in proj_dir, you need to make changes.
 
 
 def get_max_mw(path):
@@ -18,16 +21,17 @@ def get_max_mw(path):
         max_mw = 500
     else:
         max_mw = 0
-        msg = path + ' is not a resistor file. Try pressing Resistors again and selecting a csv file.'
+        msg = path + ' is not a resistor file. Try again by pressing \
+        Resistors Button and selecting a resistor csv file.'
         sg.popup_ok_cancel(msg)
     return max_mw
 
 
-def get_resistor_file_path(values):
+def get_resistor_file_path():
     # print("in function: get_resistor_file_path")
     path = sg.popup_get_file("Select correct File",
                              file_types=(('CSV', '*.csv'),),
-                             initial_folder=os.getcwd()
+                             initial_folder=proj_dir
                              )
     if path:
         window["path"].update(path)
@@ -35,58 +39,72 @@ def get_resistor_file_path(values):
     return path
 
 
-def get_candidates(values):
-    path = values["path"]
-    vin1 = values["Vin_1"]
-    # print(f'get_candidates called(path: {path} and Vin: {vin1})')
-    candidates = vdr.compute(vin1, path)
-    # print(f'Returned {len(candidates)} candidates')
+def get_candidates():
+    candidates = vdr.compute(values["Vin_1"], values["path"])
     window["candidate_table"].update(values=candidates)
 
 
-input_layout = [[
-                sg.Button("Resistors"),
-                sg.Input("", size=(80, 1), key="path"),
-                sg.Text("Vin"), sg.Input("", size=(7, 1), key="Vin_1"),
-                sg.Button("Compute"),
-                ]]
-# VD = namedtuple("VD", "Vin V1 V2 Deviance R1 R2 Pow1_mw Pow2_mw A2D")
-root = os.getcwd()
+#  ----------Layout GUI----------------------------
+
+input_group = [[sg.Button("Resistors"),
+                sg.Input("", size=(75, 1), key="path"),
+                sg.Text("Vin"), sg.Input("", size=(7, 1), enable_events=True, key="Vin_1"),
+                sg.Button("Compute")]]
+
 headings = ["Vin", "V1", "V2", "Deviance", "R1_ohms",
             "R2_ohms", "P1_mw", "P2_mw", "A2D"]
-table_layout = [[sg.Table(values=[], headings=headings, num_rows=6,
-                          auto_size_columns=False, key='candidate_table',
-                          alternating_row_color='Grey',
-                          col_widths=list(
-                              map(lambda x: 2 * (len(x) + 1), headings)),
-                          hide_vertical_scroll=True), sg.Text("Max_mw "),
-                 sg.Input("", size=(5, 1), key="max_mw"), ]]
-#  schematic = [[sg.Image(filename=root + "/VoltageDivider.png",
-schematic = [[sg.Image(filename=root + "/Schematic.png",
-                       size=(600, 335), visible=True,),
-              sg.Button("Exit")]]  # key="Schematic",
-output_layout = [[sg.Column(table_layout, scrollable=True, size=(1000, 110))]]
+table = sg.Table(values=[], headings=headings, num_rows=6,
+                 auto_size_columns=False, key='candidate_table',
+                 justification='c',
+                 alternating_row_color='Grey',
+                 col_widths=list(map(lambda x: 2 * (len(x) + 1), headings)),
+                 hide_vertical_scroll=True)
+output_group = [[table]]
 
-input_frame = sg.Frame("Inputs", input_layout, visible=True)
-output_frame = sg.Frame("Outputs", output_layout, visible=True)
-layout = [[[[input_frame]], [[output_frame]], [[schematic]]]]
+schematic_frm = sg.Frame(
+    "Schematic",
+    [[sg.Image(filename=proj_dir + "/Schematic.png")]],)
 
-# sg.Debug("Debug")
+"""
+All constraints are readonly, 
+future decision: Should the V2 constraints be set in gui or configured as globals?
+Using the GUI to set them might be instructive to the new hobbyist to see how they
+limit the number of choices for a final design.
+"""
+limit_frm = sg.Frame("Constraints",
+    [[sg.Input(vdr.v2_lo, size=(5, 1), readonly=True,),
+      sg.Text("<  V2  <"),
+      sg.Input(vdr.v2_hi, size=(5, 1), readonly=True,),
+      sg.Text("Max_mw "),
+      sg.Input("", size=(5, 1), key="max_mw", readonly=True),
+      sg.Button("Exit"), ]])
 
 
-window = sg.Window("Voltage Divider", layout,
-                   font="Courier 14", return_keyboard_events=True,
-                   resizable=True)
 
-# Event Loop, continues until Window is closed or Exit button is pressed.
+input_frame = [[sg.Frame("Inputs", input_group)]]
+output_frame = [[sg.Frame("Outputs", output_group, element_justification='left')]]
+layout = [[input_frame, output_frame, schematic_frm, limit_frm, ]]
+
+
+window = sg.Window("Voltage Divider", layout, return_keyboard_events=True, 
+                   size=(750, 500), finalize=True) #  resizable=True,
+
+"""
+ handles all events in the Event loop except sg.WinClosed and 'Exit'
+ """
+handler = {
+    'Resistors': get_resistor_file_path,
+    'Tab:805306377': window["Compute"].click,
+    'Return:603979789': window["Compute"].click,
+    'Compute': get_candidates}
+
+# print(f'----------------------Window size: {window.Size}--------------')
+
 while True:
     event, values = window.read()
+    #  print(event, values)
     if event in (sg.WIN_CLOSED, 'Exit'):
         break
-    elif event == "Resistors":
-        path = get_resistor_file_path(values)
-    elif event in ("Tab:805306377", "Return:603979789"):
-        window["Compute"].click()
-    elif event == "Compute":
-        get_candidates(values)
-    # print(event, values)
+    else:
+        handler.get(event, lambda: None)()  # if event is not in handler, defaults to None() which does nothing 
+
